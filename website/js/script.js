e0283c751bc0e58d6ea3c3feed7173fc1dd1d839
mrcordova <46950394+mrcordova@users.noreply.github.com>
2025-03-14 07:46:45 -0700

    update resume and fix grammar error

--- website/js/script.js
+++ website/js/script.js
@@ -33,11 +33,9 @@ function showError(e) {
   );
 }
 
-// for (const input of inputs) {
 inputs[0].addEventListener("input", showError);
 inputs[1].addEventListener("change", showError);
 inputs[2].addEventListener("input", showError);
-// }
 
 msgBtn.addEventListener("click", (e) => {
   e.preventDefault();
